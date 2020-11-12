f4a16cd4a19731f97043ebb6d75ee184c75d5ad1
Thomas P <thomas.perez@travaux.com>
2020-11-12 22:32:31 +0100

    Fix cs
    Signed-off-by: Thomas P <thomas.perez@travaux.com>

--- src/Plugin.php
+++ src/Plugin.php
@@ -13,31 +13,31 @@ use Symfony\Component\Finder\SplFileInfo;
 
 final class Plugin implements ICodeliciaPlugin
 {
-	/** @psalm-return list<non-empty-string> */
-	public function supportedExtensions(): array
-	{
-		return ['md', 'rst'];
-	}
+    /** @psalm-return list<non-empty-string> */
+    public function supportedExtensions(): array
+    {
+        return ['md', 'rst'];
+    }
 
-	public function canHandle(SplFileInfo $file): bool
-	{
-		return true;
-	}
+    public function canHandle(SplFileInfo $file): bool
+    {
+        return true;
+    }
 
-	public function __invoke(SplFileInfo $file, OutputFormatter $output): bool
-	{
-		foreach (Markinho::extractCodeBlocks($file->getPathname(), $file->getContents()) as $codeBlock) {
-		    if ($codeBlock->language() !== 'json') {
-			continue;
-		    }
+    public function __invoke(SplFileInfo $file, OutputFormatter $output): bool
+    {
+        foreach (Markinho::extractCodeBlocks($file->getPathname(), $file->getContents()) as $codeBlock) {
+            if ($codeBlock->language() !== 'json') {
+                continue;
+            }
 
-		    try {
-		    	json_decode($codeBlock->code(), true, 512, JSON_THROW_ON_ERROR);
-		    } catch (\Exception $ex) {
-			$output->addViolation(new Violation($codeBlock, $ex->getMessage(), 0));
+            try {
+                json_decode($codeBlock->code(), true, 512, JSON_THROW_ON_ERROR);
+            } catch (\Exception $ex) {
+                $output->addViolation(new Violation($codeBlock, $ex->getMessage(), 0));
 
-			return false;
-		    }
-		}
-	}
+                return false;
+            }
+        }
+    }
 }
